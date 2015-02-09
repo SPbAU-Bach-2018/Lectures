699ea939b1ae9ba62bd617942900c7fc89b292df
yeputons <egor.suvorov@gmail.com>
2015-02-09 23:04:34 +0300

    build.sh: now the one in template/ is main; others just call it with argument specified

--- template/build.sh
+++ template/build.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
-if [ -e *.aux ]; then
-  old_hash=`md5sum *.aux`
+ROOT_FILE=$1
+if [ -e $ROOT_FILE.aux ]; then
+  old_hash=`md5sum $ROOT_FILE.aux`
 fi
 
 while true;
 do
 	echo -e "\n\e[33m ========== Running XeLaTeX ========== \e[0m"
-	xelatex -halt-on-error -8bit -shell-escape guidelines.tex || exit 1
-	new_hash=`md5sum *.aux`
+	xelatex -halt-on-error -8bit -shell-escape $ROOT_FILE.tex || exit 1
+	new_hash=`md5sum $ROOT_FILE.aux`
 	if [ "$old_hash" == "$new_hash" ]; then
 	  break;
 	fi
