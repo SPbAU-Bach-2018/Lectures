0c91b4bfefffea10d19f3e8b90549cd64fb226a7
yeputons <egor.suvorov@gmail.com>
2015-02-10 23:39:39 +0300

    build.sh: fixed to work on Mac OS (md5 instead of md5sum)

--- template/build.sh
+++ template/build.sh
@@ -1,14 +1,23 @@
 #!/bin/bash
+function get_md5cmd() {
+  if hash md5sum >/dev/null 2>&1; then
+    echo md5sum
+  else
+    echo md5
+  fi
+}
+
+MD5CMD=$(get_md5cmd)
 ROOT_FILE=$1
 if [ -e $ROOT_FILE.aux ]; then
-  old_hash=`md5sum $ROOT_FILE.aux`
+  old_hash=`$MD5CMD $ROOT_FILE.aux`
 fi
 
 while true;
 do
 	echo -e "\n\e[33m ========== Running XeLaTeX ========== \e[0m"
 	xelatex -halt-on-error -8bit -shell-escape $ROOT_FILE.tex || exit 1
-	new_hash=`md5sum $ROOT_FILE.aux`
+	new_hash=`$MD5CMD $ROOT_FILE.aux`
 	if [ "$old_hash" == "$new_hash" ]; then
 	  break;
 	fi
