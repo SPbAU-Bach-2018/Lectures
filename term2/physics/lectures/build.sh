b646a06733dbe06ff1406378e477ebf3a7347952
yeputons <egor.suvorov@gmail.com>
2015-02-22 14:01:14 +0300

    physics: build.sh was prettified

--- term2/physics/lectures/build.sh
+++ term2/physics/lectures/build.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-templatedir=../../../template
-TEXINPUTS=.:$templatedir:$SPBAU_BACH_2018_DISK/term2/physics/lectures: /bin/bash $templatedir/build.sh lectures
+TEXINPUTS=$SPBAU_BACH_2018_DISK/term2/physics/lectures /bin/bash $templatedir/build.sh lectures
