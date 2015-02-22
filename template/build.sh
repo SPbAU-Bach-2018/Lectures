b271521563c4e794f13deaa13ce522e9fde53d77
yeputons <egor.suvorov@gmail.com>
2015-02-22 13:19:06 +0300

    template/build.sh: md5 detection was moved after Windows-specific code

--- template/build.sh
+++ template/build.sh
@@ -1,13 +1,4 @@
 #!/bin/bash
-function get_md5cmd() {
-  if hash md5sum >/dev/null 2>&1; then
-    echo md5sum
-  else
-    echo md5
-  fi
-}
-
-MD5CMD=$(get_md5cmd)
 ROOT_FILE=$1
 
 if [ ! -f "$ROOT_FILE.tex" ]; then
@@ -21,6 +12,16 @@ if [ "$OS" == "Windows_NT" ]; then
 	texify --pdf --engine=xetex --tex-option=-8bit --tex-option=-shell-escape $ROOT_FILE.tex
 	exit $?
 fi
+
+function get_md5cmd() {
+  if hash md5sum >/dev/null 2>&1; then
+    echo md5sum
+  else
+    echo md5
+  fi
+}
+
+MD5CMD=$(get_md5cmd)
 
 if [ -e $ROOT_FILE.aux ]; then
   old_hash=`$MD5CMD $ROOT_FILE.aux`
