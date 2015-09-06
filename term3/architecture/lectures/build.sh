1f536ac62818d4c49d486234679c57cd96f937fc
Egor Suvorov <egor.suvorov@gmail.com>
2015-09-05 18:51:18 -0700

    Make build scripts in term3 use correct YANDEX_DISK paths

--- term3/architecture/lectures/build.sh
+++ term3/architecture/lectures/build.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-TEXINPUTS=$SPBAU_BACH_2018_DISK/term2/calculus/lectures /bin/bash ../../../template/build.sh lectures
+TEXINPUTS=$SPBAU_BACH_2018_DISK/term3/architecture/lectures /bin/bash ../../../template/build.sh lectures
