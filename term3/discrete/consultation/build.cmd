0d888f9f0d9b4d23533e38507c8f7b658d4334ad
Egor Suvorov <egor.suvorov@gmail.com>
2016-01-18 13:33:30 +0300

    term3/discrete/consultation: fix build scripts

--- term3/discrete/consultation/build.cmd
+++ term3/discrete/consultation/build.cmd
@@ -1,2 +1,2 @@
 @echo off
-call ..\..\..\..\template\build.cmd consultation
+call ..\..\..\template\build.cmd consultation
--- term3/discrete/consultation/build.sh
+++ term3/discrete/consultation/build.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-bash ../../../../template/build.sh consultation
+bash ../../../template/build.sh consultation
