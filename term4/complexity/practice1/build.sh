4fef340dff29adee9f3c07e692074460dd1808e0
Egor Suvorov <egor.suvorov@gmail.com>
2016-02-14 16:51:20 +0300

    term4: fix some build.sh

--- term4/complexity/practice1/build.sh
+++ term4/complexity/practice1/build.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-TEXINPUTS=$SPBAU_BACH_2018_DIS/term4/complexity/practice1 /bin/bash ../../../template/build.sh practice1
+TEXINPUTS=$SPBAU_BACH_2018_DISK/term4/complexity/practice1 /bin/bash ../../../template/build.sh practice1
--- term4/discrete/practice1/build.cmd
+++ term4/discrete/practice1/build.cmd
@@ -1,3 +1,3 @@
 @echo off
-set TEXINPUTS=%SPBAU_BACH_2018_DISK%\term4\complexity\practice1
+set TEXINPUTS=%SPBAU_BACH_2018_DISK%\term4\discrete\practice1
 call ..\..\..\template\build.cmd practice1
