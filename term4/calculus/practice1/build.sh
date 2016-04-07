ad4f95177b2e5b4ab2bd9017d02b9359d8f0638c
Egor Suvorov <egor.suvorov@gmail.com>
2016-04-07 14:19:12 +0300

    term4/calculus/practice1: fix build scripts to gather pictures from ./pictures/ as well

--- term4/calculus/practice1/build.sh
+++ term4/calculus/practice1/build.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-TEXINPUTS=$SPBAU_BACH_2018_DISK/term4/calculus/practice1 /bin/bash ../../../template/build.sh practice1
+DIR=$(dirname `realpath $0`)
+TEXINPUTS=$DIR/pictures:$SPBAU_BACH_2018_DISK/term4/calculus/practice1 /bin/bash ../../../template/build.sh practice1
