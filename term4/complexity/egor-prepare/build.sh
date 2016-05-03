7dbdc15df50cc933a7a2c9522bef0adff90cb348
Egor Suvorov <egor.suvorov@gmail.com>
2016-05-03 15:37:11 +0300

    term4/complexity/egor-prepare: fix build.sh

--- term4/complexity/egor-prepare/build.sh
+++ term4/complexity/egor-prepare/build.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-TEXINPUTS=$SPBAU_BACH_2018_DISK/term4/complexity/lectures /bin/bash ../../../template/build.sh lectures
+TEXINPUTS=$SPBAU_BACH_2018_DISK/term4/complexity/egor-prepare /bin/bash ../../../template/build.sh questions
