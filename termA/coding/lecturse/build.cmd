db596163ce7177599c327e4c0d387d924761959f
Egor Suvorov <egor.suvorov@gmail.com>
2019-02-12 12:31:24 +0300

    termA/coding: add part of the first lecture

---
+++ termA/coding/lecturse/build.cmd
@@ -0,0 +1,3 @@
+@echo off
+set TEXINPUTS=%SPBAU_BACH_2018_DISK%\termA\coding\lectures
+call ..\..\..\template\build.cmd lectures
---
+++ termA/coding/lecturse/build.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+TEXINPUTS=$SPBAU_BACH_2018_DISK/termA/coding/lectures /bin/bash ../../../template/build.sh lectures
